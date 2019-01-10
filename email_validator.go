7a7889b54753c1e721e06018ec6401b8dab5260c
emptyshadow <dimon6407@gmail.com>
2019-01-10 18:45:33 +0300

    Модификация: реализован метод для валидации формата записи email

--- email_validator.go
+++ email_validator.go
@@ -1,6 +1,15 @@
 package email_validator
 
-import "bitbucket.org/EmptyShadow/smtp_use_proxy"
+import (
+	"bitbucket.org/EmptyShadow/smtp_use_proxy"
+	"errors"
+	"regexp"
+)
+
+// ErrorFormat - ошибка формата записи
+var (
+	ErrorFormat = errors.New("format is not valid")
+)
 
 // Валидатор Email адресов
 // proxy - объект прокси сервера, через который будет происходить соединение при тестовой отправки, если nil,
@@ -29,4 +38,22 @@ func NewEmailValidator(proxy *smtp_use_proxy.Proxy) (*EmailValidator, error) {
 // Get proxy
 func (v *EmailValidator) GetProxy() (*smtp_use_proxy.Proxy) {
 	return v.proxy
+}
+
+// Проверка формата записи email
+// email - адрес, который требуется проверить
+//
+// Check email entry format
+// email - the address you want to check
+func (v *EmailValidator) CheckFormat(email string) (error) {
+	emailRegexp, err := regexp.Compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	if err != nil {
+		return err
+	}
+
+	if !emailRegexp.MatchString(email) {
+		return ErrorFormat
+	}
+
+	return nil
 }
